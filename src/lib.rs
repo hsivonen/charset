d7c6e03f666d626142d3bd39f32f417a9a483b4f
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-06-13 10:11:38 +0100

    Bump bincode to 1.3.3

--- src/lib.rs
+++ src/lib.rs
@@ -1079,7 +1079,7 @@ mod tests {
         let deserialized: Demo = serde_json::from_str(&serialized).unwrap();
         assert_eq!(deserialized, demo);
 
-        let bincoded = bincode::serialize(&demo, bincode::Infinite).unwrap();
+        let bincoded = bincode::serialize(&demo).unwrap();
         let debincoded: Demo = bincode::deserialize(&bincoded[..]).unwrap();
         assert_eq!(debincoded, demo);
     }
@@ -1098,7 +1098,7 @@ mod tests {
         let deserialized: Demo = serde_json::from_str(&serialized).unwrap();
         assert_eq!(deserialized, demo);
 
-        let bincoded = bincode::serialize(&demo, bincode::Infinite).unwrap();
+        let bincoded = bincode::serialize(&demo).unwrap();
         let debincoded: Demo = bincode::deserialize(&bincoded[..]).unwrap();
         assert_eq!(debincoded, demo);
     }
